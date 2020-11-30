d12404032750c5df7c04a7bd5bf2a6ae765104cc
Danylo <danyloboshagora@gmail.com>
2020-11-30 12:09:06 +0100

    added dummy data

--- src/sample/dal/MockData.java
+++ src/sample/dal/MockData.java
@@ -1,12 +1,28 @@
 package sample.dal;
 
+import sample.be.Playlist;
 import sample.be.Song;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MockData implements dataAccessnterface {
+
+    public List<Song> allSongs = new ArrayList<>();
+
     @Override
     public List<Song> getAllSongs() {
-        return null;
+        allSongs.add(s1);
+        allSongs.add(s2);
+        allSongs.add(s3);
+        return allSongs;
     }
+
+    Playlist zori = new Playlist("Zori");
+    Playlist pathetic = new Playlist("Pathetic");
+
+    Song s1 = new Song("theSong1", "Kalinka", "Artist 1", 289, zori);
+    Song s2 = new Song("Makaroni", "Hang", "Tifest", 309, zori);
+    Song s3 = new Song("Lasagna", "Uber Eat", "Tifest", 320, pathetic);
+
 }
