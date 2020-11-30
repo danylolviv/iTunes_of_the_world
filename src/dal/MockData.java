0dbb8c59f5d2e982e7afc8e121a5c6a23d29bc56
Danylo <danyloboshagora@gmail.com>
2020-11-30 11:27:42 +0100

    added enteties and created data interface

---
+++ src/dal/MockData.java
@@ -0,0 +1,12 @@
+package dal;
+
+import be.Song;
+
+import java.util.List;
+
+public class MockData implements dataAccessnterface {
+    @Override
+    public List<Song> getAllSongs() {
+        return null;
+    }
+}
---
+++ src/dal/dataAccessnterface.java
@@ -0,0 +1,12 @@
+package dal;
+
+import be.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface dataAccessnterface {
+    public List<Song> getAllSongs();
+}
+
+
