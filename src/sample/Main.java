74ccd828f72893898ef63c83d093945e96b46e44
Marco Gabel <marcgabel@live.dk>
2020-12-09 10:48:24 +0100

    THIS VERSION WORKS!!!!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
-
+ //this works
 
     public static void main(String[] args) {
         launch(args);
