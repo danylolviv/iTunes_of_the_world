55930bdd5cd72135bc5021007d8240a320861fe3
Marco Gabel <marcgabel@live.dk>
2020-12-15 19:13:22 +0100

    Revert "Merge branch 'main' of https://github.com/danylolviv/iTunes_of_the_world into main"
    This reverts commit b225a2bced15c509b50c91b9d35b7ce584d3e5f1, reversing
    changes made to 396cdb7c84ce185977d939311a20d22305ac908c.

--- src/sample/bll/SongManager.java
+++ src/sample/bll/SongManager.java
@@ -5,7 +5,6 @@ import sample.be.Genre;
 import sample.be.Song;
 import sample.dal.DAOSong;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SongManager {
@@ -16,9 +15,6 @@ public class SongManager {
     public List<Song> getAllSongs(){
         return daoSong.getAllSongs();
     }
-    public List<Song> searchTheSongs(String searchQuery){ return daoSong.searchForTheSongs(searchQuery); }
-
-
     public int getNextId() {
         List<Song> songs = getAllSongs();
         return songs.stream().filter(song -> song.getID() != songs.indexOf(song)).findFirst().map(songs::indexOf).orElse(songs.size());
